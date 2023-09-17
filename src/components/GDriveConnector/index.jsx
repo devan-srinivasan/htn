e4a881f9f1d3a34dcedf5a7b3c2dbcc5125acf56
Michelle Chernyi <michellechernyi@v1044-wn-rt-a-142-97.campus-dynamic.uwaterloo.ca>
2023-09-16 20:08:36 -0400

    added frontend

---
+++ src/components/GDriveConnector/index.jsx
@@ -0,0 +1,45 @@
+import Header from '../Header'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import gdrive from './gdrive_logo.png'
+import {Link, Routes, Route, useNavigate} from 'react-router-dom';
+import style from './style.css'
+
+
+function DriveConnector() {
+
+  const navigate = useNavigate();
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    // fetch(`http://localhost:3001/save-note"`)
+    // .then(response => response.json())
+    // .then(body => {
+    //   console.log(body);
+    //   })
+
+    navigate('/main');
+  }
+
+  return (
+    <div>
+      <Header />
+      <Form className="formWrapper" onSubmit={handleSubmit}>
+        <button type="submit" class="btn btn-primary">
+          Connect to Google Drive
+          <img src={gdrive} className="drive-logo" alt="logo"/>
+      </button>
+      </Form>
+    </div>
+  );
+}
+
+export default DriveConnector;
