d43bf4ca4aab877bf299f31c0fa51faa05e513a0
adi-dew <aditya.dewan124@gmail.com>
2023-09-16 20:46:55 -0400

    V.4.0 - FIXED CROPPING PROCESSING + IMPLEMENTING GOOGLE DOCS FORMATTING CHANGES.

--- BACKEND/server.py
+++ BACKEND/server.py
@@ -7,9 +7,10 @@ import cv2
 import pyautogui
 from PIL import Image
 from pytesseract import pytesseract
-# from utils import createGoogleDoc
+from utils import createGoogleDoc
 from utils import extractText, generateNotes
 import time
+import math
 
 #Init server
 NOTE_SERVER = Flask(__name__)
@@ -45,40 +46,50 @@ def save_note():
     img = pyautogui.screenshot()
     #Convert to numpy array and then to a PIL image that can be written to disk
     saved_img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
-    #Write complete screenshot to disk
-    PIL_saved_img = Image.fromarray(saved_img.astype("uint8"), "RGB")
     complete_screenshot_filename = "COMPLETE_SCREENSHOT_" + str(len(COMPLETE_NOTE_SCREENSHOT_NAMES)) + ".png"
-    PIL_saved_img.save("COMPLETE_SCREENSHOT_" + str(len(COMPLETE_NOTE_SCREENSHOT_NAMES)) + ".png")
-    #Append name to screenshot
-    COMPLETE_NOTE_SCREENSHOT_NAMES.append(complete_screenshot_filename)
-    #Extract text
+    cv2.imwrite(complete_screenshot_filename, saved_img)
+    #Get complete text from image
     complete_text = extractText(image_file_path = complete_screenshot_filename)
-    print("COMPLETED TEXT:", complete_text)
+    #Append name to screenshot list
+    COMPLETE_NOTE_SCREENSHOT_NAMES.append(complete_screenshot_filename)
+
     #Get gaze direction from AdHawk dataset and crop the image accordingly
-    #9-quadrant-based system9
+    #9-quadrant-based system
     #horizontal_direction, vertical_direction = getScreenGazeDirection()
-    vertical_direction = "down"
+    vertical_direction = "up"
     horizontal_direction = "right"
     #Get center of region the user is looking at
     center_point = DIRECTION_MATRIX[vertical_directions.index(vertical_direction)][horizontal_directions.index(horizontal_direction)]
     #Crop the screen shot to ~60% of the screen size around the center point
     #Crop percentage for vertical and horizontal directions
-    vertical_crop_percent = 0.6
-    horizontal_crop_percent = 0.25
+    vertical_crop_percent = 0.5
+    horizontal_crop_percent = 0.3
+    #convert saved_img to PIL image
+    PIL_Image = Image.fromarray(saved_img)
     #Left, top, right, bottom
-    cropped_img = PIL_saved_img.crop((center_point[1] - (vertical_crop_percent * screen_size[1]), center_point[0] - (horizontal_crop_percent * screen_size[0]),
-                                      center_point[1] + (vertical_crop_percent * screen_size[1]), center_point[0] + (horizontal_crop_percent * screen_size[0])))
+    left_crop = max(center_point[1] - (vertical_crop_percent * screen_size[0]), 0)
+    top_crop = max(center_point[0] - (horizontal_crop_percent * screen_size[1]), 0)
+    right_crop = min(center_point[1] + (vertical_crop_percent * screen_size[0]), screen_size[0])
+    down_crop = min(center_point[0] + (horizontal_crop_percent * screen_size[1]), screen_size[1])
+    cropped_img = PIL_Image.crop((left_crop, top_crop,
+                                  right_crop, down_crop))
     #Save to disk
     img_name = "PARTIAL_NOTE_" + str(len(PARTIAL_NOTE_SCREENSHOT_NAMES)) + ".png"
     cropped_img.save(img_name)
     #Get cropped region text
     cropped_region_text = extractText(img_name)
-    #Get LLM-augmented notes - do this based on the full generation
-    augmented_notes_text = generateNotes(note_text = complete_text)
-    print("AUGMENTED NOTES TEXT:", augmented_notes_text)
+    #Log
+    print("CROPPED REGION TEXT:", cropped_region_text)
+    #Get LLM-augmented notes - do this based on the text retreived from the complete image and the cropped iamge
+    augmented_complete_notes_text = generateNotes(note_text = complete_text)
+    augmented_cropped_notes_text = generateNotes(note_text = cropped_region_text)
+    print("AUGMENTED COMPLETED NOTES:", augmented_complete_notes_text)
+    print("AUGMENTED CROPPED NOTES:",  augmented_cropped_notes_text)
+    #Prepare augmented notes to be written on Google Docs
+    augmented_notes = f"Our notes for this slide:\n\n{augmented_complete_notes_text}\n\nWhat we think you were most interested in:\n\n{augmented_cropped_notes_text}"
     #Save image and text to Google Docs
-    # createGoogleDoc(extracted_text = augmented_notes_text, image_filepath = img_name)
-    message = "We've successfully saved this note to Google Docs."
+    createGoogleDoc(extracted_text = augmented_notes, image_filepath = img_name)
+    message = "We've successfully enhanced and saved your notes to Google Docs."
     return {"success_message": message}
 
 #Serve
